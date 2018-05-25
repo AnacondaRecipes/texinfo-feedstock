cdad4cc729a2b982a79ab40bc7cda16b3f7d34eb
Ray Donnelly <mingw.android@gmail.com>
2018-05-26 00:22:13 +0100

    Stop using system perl, add missing deps for osx, run autoreconf etc etc

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX PERL='/usr/bin/env perl'
+autoreconf -vfi
 
-make
-# OS X fails on `ii-0041-test` and `ii-0050-test` 2 out of 57.
-if [[ $(uname) == Linux ]]; then
+./configure --prefix=${PREFIX}
+
+make -j${CPU_COUNT} ${VERBOSE_AT}
+if [[ ${target_platform} != osx-64 ]]; then
+  # https://github.com/NixOS/nixpkgs/issues/2338
+  # https://angelika.me/2016/12/10/compressed-text-files/
+  # /usr/bin/zdiff is used in the testsuite and the one Apple provides
+  # does not work with bash (nice work Apple). We could provide
+  # zutils instead: http://download.savannah.gnu.org/releases/zutils/
+  # .. but for now ..
   make check
 fi
 make install
