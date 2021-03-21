260c433265f6270ac1f85b862bcdc058a9b05924
leej3 <leej3@quansight.com>
2021-03-21 10:40:20 +0000

    add stopgap solution for missing autopoint from gettext
    we do not want to build the gettext package as it is part of glibc, however it appears we do not have auotpoint without
    it. for now we can avoid the configure failure by just altering the error behavior, this is not an ideal solution though.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# A missing autopoint raises a non zero exit status during the configuration.
+# This is part of gettext... which we don't build but should be part of glibc
+# on linux. If execution is not stopped by this the package goes on to build
+# just fine... not sure whether the configure stage can be tweaked to not
+# insist that this functionality is present, even if just for a bootstrapping
+# build of texinfo...
+set +e
 autoreconf -vfi
+set -e
 
 # We do this to prevent configure finding perl in the host prefix and
 # using it in shebangs. /usr/bin/env <thing> is always the right thing
